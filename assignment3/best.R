8dd02e4a723c294391126e20bc017ae28f543085
Windex <glen.greer@gmail.com>
2015-08-28 20:11:11 -0600

    Part 3 & 4

--- assignment3/best.R
+++ assignment3/best.R
@@ -5,17 +5,15 @@ best <- function(state, outcome) {
     
     ## Check that state and outcome are valid
     s <- data[data$State == state,]
-    if (0 == nrow(s)) {
+    if (0 == nrow(s))
         stop("invalid state")
-    }
     valid_outcomes <-
         c(
             "heart attack" = 11, "heart failure" = 17, "pneumonia" = 23
         )
     outcol <- names(s)[valid_outcomes[outcome]]
-    if (is.na(outcol)) {
+    if (is.na(outcol))
         stop("invalid outcome")
-    }
     
     ## Return hospital name in that state with lowest 30-day death rate
     s[,outcol] <- suppressWarnings(as.numeric(s[,outcol]))
